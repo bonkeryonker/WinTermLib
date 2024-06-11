26b31c4a82e63cd6b9ba5004cf957f2bb2f136b9
Gabe Yonker <gabeyonker@gmail.com>
2024-06-11 11:00:54 -0500

    Fixed bug with internal window buffer. Refactored code to use _chiBuffer as the frame buffer rather than a separate window handle.

--- WinTermLib/WinTerm.cpp
+++ WinTermLib/WinTerm.cpp
@@ -1,70 +1,104 @@
 #include "WinTerm.h"
 
+//TODO: Reimplement everything from github and push
+//TODO: Additional TODOs in Constructor
+//TODO: Throw errors if setting buffer/window size fails instead of just returning (Also in updateBufferInfo(), and writeCharANSI())
+
 namespace WinTermLib
 {
-	WinTerm::WinTerm(short TermSizeX, short TermSizeY)
-	{
-		///	TODO: 
-		/// Allow for custom window titles to be passed in via constructor
+	/*
+		==PUBLIC METHODS==
+	*/
 
-		// Set up handles for read/write
+	WinTerm::WinTerm(short TermSizeX, short TermSizeY, bool matchBufferToWindowSize)
+	{
+		//TODO: See if there's a hacky solution to disallowing user to resize window (AFAIK this is impossible with win32 console APIs as it falls under the purview of the user's terminal emulator)
+		//TODO: Reimplement everything from github and push
+		//TODO: Allow for custom window titles
+		//TODO: Overload constructor to allow for buffers larger than window size (Remove line below this one)
+		matchBufferToWindowSize = true;
+
 		_wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
 		_rHnd = GetStdHandle(STD_INPUT_HANDLE);
 
-		// Set up handle for framebuffer for buffered character display
-		// (https://learn.microsoft.com/en-us/windows/console/reading-and-writing-blocks-of-characters-and-attributes?redirectedfrom=MSDN)
-		_frameBuffer = CreateConsoleScreenBuffer(
-			GENERIC_READ |	// read/write access
-			GENERIC_WRITE,
-			FILE_SHARE_READ |
-			FILE_SHARE_WRITE,	// Shared
-			NULL,	// Default security attributes
-			CONSOLE_TEXTMODE_BUFFER,	// Must be TEXTMODE
-			NULL);	// Reserved; must be NULL
-		
+		// Set window title
+		SetConsoleTitle(TEXT("Win32 Console Control with WinTermLib!"));
 
-		// Change window title
-		SetConsoleTitle(TEXT("Win32 Console Example using WinTermLib!"));
+		// Get initial csbi
+		updateBufferInfo();
 
-		//Set up required window size
-		short tempY = TermSizeY - 1;	// Decrement by one to compensate for 0,0 origin
-		short tempX = TermSizeX - 1;	// Decrement by one to compensate for 0,0 origin
-		_windowSize = { 0, 0, tempY, tempX };
-		SetConsoleWindowInfo(_wHnd, TRUE, &_windowSize);
-		//SetConsoleWindowInfo(_frameBuffer, TRUE, &_windowSize);
+		// Define the new window size ensuring it does not exceed the maximum window size
+		short tempY = min(TermSizeY - 1, _csbi.dwMaximumWindowSize.Y - 1);
+		short tempX = min(TermSizeX - 1, _csbi.dwMaximumWindowSize.X - 1);
+		_windowSize = { 0, 0, tempX, tempY };
 
-		// Convert window size to the length of a single-dimensional array representing all characters on screen
-		// (Will be size of _chiBuffer internally)
-		_chiBufSize = TermSizeY * TermSizeX;
-		_chiBuffer.resize(_chiBufSize);
-
-
-		// Change the internal buffer size to match window size
-		_bufferSize = { TermSizeY, TermSizeX };
-		SetConsoleScreenBufferSize(_wHnd, _bufferSize);
-	}
-
-	void WinTerm::fillScreenTest(CHAR_INFO testChar, HANDLE windowHandle)
-	{
-		for (int y = 0; y < _bufferSize.Y; ++y)
-		{
-			for (int x = 0; x < _bufferSize.X; ++x)
-			{
-				_testChiBuf[x + _bufferSize.X * y] = testChar;
-			}
+		// Set window size
+		if (!SetConsoleWindowInfo(_wHnd, TRUE, &_windowSize)) {
+			std::cerr << "SetConsoleWindowInfo failed with error: " << GetLastError() << std::endl;
+			return;
 		}
 
-		// Write the characters:
-		WriteConsoleOutputA(windowHandle, _testChiBuf, _bufferSize, {0,0}, &_windowSize);
+		// Create window buffer as COORD for easy access
+		_bufferDims = {
+			TermSizeX,
+			TermSizeY
+		};
+
+		// Set window buffer size
+		if (!SetConsoleScreenBufferSize(_wHnd, _bufferDims)) {
+			std::cerr << "SetConsoleScreenBufferSize failed with error: " << GetLastError() << std::endl;
+			return;
+		}
+		updateBufferInfo();
+
+		// Window Size, Title, and Buffer setup complete!
+		// Declare and initialize _chiBuffer
+		_chiBufSize = _bufferDims.X * _bufferDims.Y;
+		_chiBuffer = new CHAR_INFO[_chiBufSize];
+
+		// Initialize contents of frame buffer to blank characters with black background (To avoid crashes when the user calles flush() with an empty _chiBuffer)
+		fillBuffer(' ', DefaultCharacterAttributes);
+	}
+
+
+	void WinTerm::writeCharANSI(char asciiChar, COORD charPos, WORD attributes)
+	{
+		// Verify that charPos is within the range of window buffer
+		if ((charPos.X < 0 || charPos.X >= _bufferDims.X) || (charPos.Y < 0 || charPos.Y >= _bufferDims.Y))
+		{
+			std::cerr << "writeCharANSI failed." << std::endl
+				<< "Buffer dimensions {" << _bufferDims.X << "," << _bufferDims.Y << "} (Tried to write to [" << charPos.X << "][" << charPos.Y << "])" << std::endl;
+			return;
+		}
+
+		// Convert passed asciiChar to CHAR_INFO
+		CHAR_INFO chi;
+		chi.Char.AsciiChar = asciiChar;
+		chi.Attributes = attributes;
+
+		// Convert charPos coordinates to 1 dimensional _chiBuffer index
+		int i = charPos.Y * _bufferDims.X + charPos.X;
+
+		//	Write chi to _chiBuffer @ index i
+
+		_chiBuffer[i] = chi;
+	}
+
+	void WinTerm::fillBuffer(char asciiChar, WORD attributes)
+	{
+		CHAR_INFO chi;
+		chi.Char.AsciiChar = asciiChar;
+		chi.Attributes = attributes;
+		for (int i = 0; i < _chiBufSize; i++)
+			_chiBuffer[i] = chi;
 	}
 
 	void WinTerm::flush()
 	{
-		// Copy contents of _framebuffer to _wHnd
-		saveHandleToBuffer(_frameBuffer);
-		copyBufferToHandle(_wHnd);
-		// Copy contents of _wHnd back to _framebuffer
-		saveHandleToBuffer(_frameBuffer);
+		// The write area is a SMALL_RECT is the entire window buffer, and charPos is the coordinate of the inital character to write (Should always be 0,0 in this case)
+		SMALL_RECT writeArea = { 0, 0, _bufferDims.X, _bufferDims.Y };
+		COORD charPos = { 0, 0 };
+		WriteConsoleOutputA(_wHnd, _chiBuffer, _bufferDims, charPos, &writeArea);
 	}
 
 	HANDLE WinTerm::getFrameBuffer()
@@ -72,34 +106,30 @@ namespace WinTermLib
 		return _frameBuffer;
 	}
 
-	int WinTerm::saveHandleToBuffer(HANDLE windowHandle)
+	COORD WinTerm::getMaxWindowSize()
 	{
-		BOOL fSuccess = ReadConsoleOutput(
-			windowHandle,	// Screen buffer to read from (stdout)
-			_chiBuffer.data(),	// Temp buffer to copy into (.data() refers to underlying array used by vector)
-			_bufferSize,	// col-row size of _chiBuffer
-			{0,0},	// Top left dest. cell in _chiBuffer
-			&_windowSize);	// Screen buffer source rectangle
-
-		if (!fSuccess)
-		{
-			printf("ReadConsoleOutput failed - (%d)\n", GetLastError());
-			return 1;
+		// First get the current Console Screen Buffer Info struct
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+		if (!GetConsoleScreenBufferInfo(_wHnd, &csbi)) {
+			std::cerr << "GetConsoleScreenBufferInfo failed with error: " << GetLastError() << std::endl;
+			return { -1,-1 };
 		}
+		return csbi.dwMaximumWindowSize;
+
 	}
 
-	int WinTerm::copyBufferToHandle(HANDLE windowHandle)
+	/*
+		==PRIVATE METHODS==
+	*/
+
+	BOOL WinTerm::updateBufferInfo()
 	{
-		BOOL fSuccess = WriteConsoleOutput(
-			windowHandle,	// Screen buffer to write to
-			_chiBuffer.data(),	// Buffer to copy from
-			_bufferSize,	// col-row size of _chiBuffer
-			{ 0,0 },	// Top left dest. cell in _chiBuffer
-			&_windowSize);	//Screen buffer source rectangle
-		if (!fSuccess)
-		{
-			printf("WriteConsoleOutput failed - (%d)\n", GetLastError());
-			return 1;
+		// Get the updated console screen buffer info after resizing the buffer
+		if (!GetConsoleScreenBufferInfo(_wHnd, &_csbi)) {
+			std::cerr << "GetConsoleScreenBufferInfo failed with error: " << GetLastError() << std::endl;
+			return FALSE;
 		}
+		else
+			return TRUE;
 	}
 }
--- WinTermLib/WinTerm.h
+++ WinTermLib/WinTerm.h
@@ -6,6 +6,7 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <Windows.h>
 #include <tchar.h>
 #include <string>
@@ -16,24 +17,28 @@ namespace WinTermLib
 	class WinTerm
 	{
 	public:
-		WinTerm(short TermSizeX, short TermSizeY);
-		void writeCharANSI(char asciiChar, WORD attributes = DefaultCharacterAttributes, COORD charPos = { 0,0 });
-		void fillScreenTest(CHAR_INFO testChar, HANDLE windowHandle = GetStdHandle(STD_OUTPUT_HANDLE));
-		void flush();
+		WinTerm(short TermSizeX, short TermSizeY, bool matchBufferToWindowSize = false);
+		~WinTerm() { delete[] _chiBuffer; } // Probably unnecessary, I think _chiBuffer will be automatically freed when WinTerm goes out of scope? Look into this more!
+		void writeCharANSI(char asciiChar, COORD charPos = { 0,0 }, WORD attributes = DefaultCharacterAttributes);
+		void fillBuffer(char asciiChar, WORD attributes = DefaultCharacterAttributes);	// Fill _chiBuffer entirely with the same character
+		void flush();	// Display contents of _chiBuffer to screen (_wHnd)
 		HANDLE getFrameBuffer();
+		COORD getMaxWindowSize();
+
 		static void clearScreen() { system("cls"); };
 		static const WORD DefaultCharacterAttributes = 15U; // White Text on Black Background
 	private:
+		BOOL updateBufferInfo();	// Update _csbi with latest screen buffer info
+
 		HANDLE _frameBuffer;	// Handle for modifying console contents before writing to stdout
 		HANDLE _wHnd;	// Standard Output Write Handle
 		HANDLE _rHnd;	// Standard Input Read Handle
-		SMALL_RECT _windowSize;
-		COORD _bufferSize;
-		CHAR_INFO _testChiBuf[160 * 90];
-		std::vector<CHAR_INFO> _chiBuffer;	// Temporary buffer to store CHAR_INFO data. Used to read/write between _framebuffer and stdout
-		size_t _chiBufSize;
+		CONSOLE_SCREEN_BUFFER_INFO _csbi;	// Struct to hold data about current Screen Buffer. Not guaranteed to be up-to-date, refresh by calling updateBufferInfo()
 
-		int saveHandleToBuffer(HANDLE windowHandle = GetStdHandle(STD_OUTPUT_HANDLE));	// Method to copy the contents of the current console screen to the _frameBuffer variable (via _chiBuffer)
-		int copyBufferToHandle(HANDLE windowHandle = GetStdHandle(STD_OUTPUT_HANDLE));	// Method to copy the contents of the framebuffer to the current console screen
+		SMALL_RECT _windowSize;	// Size of window
+
+		COORD _bufferDims;	// COORD dimensions of the internal window buffer. Used to iterate through the single dimension _chiBuffer array as though it were 2d
+		size_t _chiBufSize;	// Number of elements in _chiBuffer
+		CHAR_INFO* _chiBuffer;	// Effectively acts as a frame buffer, written to by all writeChar functions. Contents will be displayed upon calling flush()
 	};
 }
